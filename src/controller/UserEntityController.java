796e07af96af32da3e05e81db12e32c7da6dd498
ChloeFancy <773889885@qq.com>
2018-07-02 00:27:44 +0800

    用户登录

--- src/controller/UserEntityController.java
+++ src/controller/UserEntityController.java
@@ -1,14 +1,42 @@
 package controller;
 
 import DAO.Impl.BaseDAOImpl;
+import DAO.Impl.UserEntityDAOImpl;
 import model.UserEntity;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import util.BasicResponse;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/student")
+@RequestMapping("/user")
 @CrossOrigin("http://localhost:8081")
 public class UserEntityController extends BaseController<UserEntity> {
+    ApplicationContext context =
+            new ClassPathXmlApplicationContext("../applicationContext.xml");
+    //此路径是在生成的class文件（out/artifacts/DeepView_war_exploded/WEB-INF/classes/controller/BaseController.class）中的相对路径
+    UserEntityDAOImpl userEntityDAO = (UserEntityDAOImpl) context.getBean("UserEntityDAOImpl");
 
+    @RequestMapping(value="/login",method = {RequestMethod.POST})
+    public @ResponseBody BasicResponse login(
+            @RequestParam(value="name") String name,
+            @RequestParam(value="password") String password) {
+        BasicResponse response = new BasicResponse();
+        response.setResCode("-1");
+        response.setResMsg("Error");
+        try{
+            boolean login = userEntityDAO.login(name, password);
+            if(login){
+                response.setResCode("1");
+                response.setResMsg("success");
+            }
+            return response;
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return response;
+    }
 }
